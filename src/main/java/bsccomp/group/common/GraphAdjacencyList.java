ab24ecbf04a045836ab97bd084ef4dac7d393ffb
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-11-08 00:38:00 +0530

    remove redundant public and raw usage

--- src/main/java/bsccomp/group/common/GraphAdjacencyList.java
+++ src/main/java/bsccomp/group/common/GraphAdjacencyList.java
@@ -3,8 +3,8 @@ package bsccomp.group.common;
 import java.util.HashSet;
 
 public interface GraphAdjacencyList<T> {
-    public void addEdge(T node);
-    public void addNodeAndWeightConnected(T vertex,T destinationNode, int weight);
-    public HashSet findAllDestinationNodeAndWeightsToAVertex(T node);
-    public void printAdjacencyList();
+    void addEdge(T node);
+    void addNodeAndWeightConnected(T vertex,T destinationNode, int weight);
+    HashSet<?> findAllDestinationNodeAndWeightsToAVertex(T node);
+    void printAdjacencyList();
 }
