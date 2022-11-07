955212bdc577c10ac5252ea606949dad85a48455
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-11-07 18:36:09 +0530

    Edit gameManager

--- src/main/java/bsccomp/group/manager/GameManager.java
+++ src/main/java/bsccomp/group/manager/GameManager.java
@@ -1,4 +1,18 @@
 package bsccomp.group.manager;
 
+import bsccomp.group.common.GraphAdjacencyListImpl;
+import bsccomp.group.identifyminimumconnectors.IdentifyMinimumConnectors;
+
 public class GameManager {
+    // need to manage object states (array-list, hash-set)
+    // need a function to find-maximum-profit
+    // need a function to identify-shortest-path
+    // need a function to identify-minimum-connectors
+    public GameManager() {
+    }
+
+    public void identifyMinimumConnectors(){
+        IdentifyMinimumConnectors minimumConnectors = new IdentifyMinimumConnectors(new GraphAdjacencyListImpl());
+        minimumConnectors.findMinimumConnectors();
+    }
 }
