1e662789e4287521c893d6f5844cee2488db01fd
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-11-10 01:35:12 +0530

    add Vertex

---
+++ src/main/java/bsccomp/group/common/Vertex.java
@@ -0,0 +1,44 @@
+package bsccomp.group.common;
+
+import java.util.Objects;
+
+public class Vertex {
+    private String name;
+    private boolean isVisited = false;
+
+    public Vertex() {
+    }
+
+    public Vertex(String name) {
+        this.name = name;
+    }
+
+    public boolean isVisited() {
+        return isVisited;
+    }
+
+    public void setVisited(boolean visited) {
+        isVisited = visited;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Vertex vertex = (Vertex) o;
+        return Objects.equals(name, vertex.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
