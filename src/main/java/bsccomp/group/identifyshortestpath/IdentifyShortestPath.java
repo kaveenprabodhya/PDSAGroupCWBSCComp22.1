3e0fa9c52142099172a8e72e9f419ffb51314946
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-11-10 02:37:43 +0530

    extend from base and add displayGraph

--- src/main/java/bsccomp/group/identifyshortestpath/IdentifyShortestPath.java
+++ src/main/java/bsccomp/group/identifyshortestpath/IdentifyShortestPath.java
@@ -1,4 +1,17 @@
 package bsccomp.group.identifyshortestpath;
 
-public class IdentifyShortestPath {
+import bsccomp.group.common.Base;
+import bsccomp.group.common.GraphAdjacencyList;
+import bsccomp.group.common.models.Vertex;
+
+public class IdentifyShortestPath extends Base {
+    private final GraphAdjacencyList<Vertex> adjacencyList;
+
+    public IdentifyShortestPath(GraphAdjacencyList<Vertex> adjacencyList) {
+        this.adjacencyList = adjacencyList;
+    }
+
+    public void displayGraph(){
+        this.adjacencyList.printAdjacencyList();
+    }
 }
