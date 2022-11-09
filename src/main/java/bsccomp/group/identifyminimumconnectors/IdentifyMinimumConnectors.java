a2b6b4f917aa26dfb67a0054e899e1ced8b5a103
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-11-10 01:34:59 +0530

    change String type to Vertex and remove create method

--- src/main/java/bsccomp/group/identifyminimumconnectors/IdentifyMinimumConnectors.java
+++ src/main/java/bsccomp/group/identifyminimumconnectors/IdentifyMinimumConnectors.java
@@ -2,30 +2,22 @@ package bsccomp.group.identifyminimumconnectors;
 
 import bsccomp.group.common.Base;
 import bsccomp.group.common.GraphAdjacencyList;
+import bsccomp.group.common.Vertex;
 
 public class IdentifyMinimumConnectors extends Base {
-    private final GraphAdjacencyList<String> adjacencyList;
+    private final GraphAdjacencyList<Vertex> adjacencyList;
 
-    public IdentifyMinimumConnectors(GraphAdjacencyList<String> adjacencyList) {
+    public IdentifyMinimumConnectors(GraphAdjacencyList<Vertex> adjacencyList) {
         this.adjacencyList = adjacencyList;
     }
 
-    private void createMinimumConnectorsGraph(){
-        for (int i = 0; i < 10; i++) {
-            String randCity = this.getRandomCity();
-            this.adjacencyList.addEdge(randCity);
-            int count = (int) (Math.random()*(5-1+1)+1);
-            for (int j = 0; j < count; j++) {
-                this.adjacencyList.addNodeAndWeightConnected(randCity, this.getRandomCity(randCity),
-                    this.getRandomDistance());
-            }
-            this.clearSecondTempCityList();
-        }
-        this.clearTempCityList();
-        this.adjacencyList.printAdjacencyList();
-    }
-
     public void findMinimumConnectors(){
-        this.createMinimumConnectorsGraph();
+        // create a graph for find minimum connectors
+        this.createGraph(adjacencyList);
+        this.adjacencyList.printAdjacencyList();
+        // select the shortest edge connected to the vertex
+
+        // select the shortest edge connect to any vertex already connected
+        // Repeat until all vertices have been connected
     }
 }
