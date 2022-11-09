58ba9d1f576dfa817dff1d5a0eaf1a599f292091
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-11-10 02:37:00 +0530

    select the vertex

--- src/main/java/bsccomp/group/identifyminimumconnectors/IdentifyMinimumConnectors.java
+++ src/main/java/bsccomp/group/identifyminimumconnectors/IdentifyMinimumConnectors.java
@@ -2,7 +2,10 @@ package bsccomp.group.identifyminimumconnectors;
 
 import bsccomp.group.common.Base;
 import bsccomp.group.common.GraphAdjacencyList;
-import bsccomp.group.common.Vertex;
+import bsccomp.group.common.models.Vertex;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class IdentifyMinimumConnectors extends Base {
     private final GraphAdjacencyList<Vertex> adjacencyList;
@@ -14,7 +17,9 @@ public class IdentifyMinimumConnectors extends Base {
     public void findMinimumConnectors(){
         // create a graph for find minimum connectors
         this.createGraph(adjacencyList);
-        this.adjacencyList.printAdjacencyList();
+        // select the vertex
+        Map.Entry<Vertex, HashMap<Vertex, Integer>> mapEntry = this.adjacencyList.get().entrySet().iterator().next();
+        Vertex vertex = mapEntry.getKey();
         // select the shortest edge connected to the vertex
 
         // select the shortest edge connect to any vertex already connected
