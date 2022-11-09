04ad4192eb0283eff6a1fed113e38e1d84503cf9
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-11-10 01:34:21 +0530

    change String type to Vertex

--- src/main/java/bsccomp/group/common/GraphAdjacencyListImpl.java
+++ src/main/java/bsccomp/group/common/GraphAdjacencyListImpl.java
@@ -3,25 +3,25 @@ package bsccomp.group.common;
 import java.util.HashMap;
 import java.util.HashSet;
 
-public class GraphAdjacencyListImpl implements GraphAdjacencyList<String> {
-    protected HashMap<String, HashMap<String, Integer>> adjacencyList;
+public class GraphAdjacencyListImpl implements GraphAdjacencyList<Vertex> {
+    protected HashMap<Vertex, HashMap<Vertex, Integer>> adjacencyList;
 
     public GraphAdjacencyListImpl() {
         adjacencyList = new HashMap<>();
     }
 
     @Override
-    public void addEdge(String node) {
+    public void addEdge(Vertex node) {
         adjacencyList.put(node, new HashMap<>());
     }
 
     @Override
-    public void addNodeAndWeightConnected(String vertex, String destinationNode, int weight) {
+    public void addNodeAndWeightConnected(Vertex vertex, Vertex destinationNode, int weight) {
         adjacencyList.get(vertex).put(destinationNode, weight);
     }
 
     @Override
-    public HashSet<?> findAllDestinationNodeAndWeightsToAVertex(String node) {
+    public HashSet<?> findAllDestinationNodeAndWeightsToAVertex(Vertex node) {
         for (int i = 0; i < adjacencyList.size(); i++) {
             if (adjacencyList.containsKey(node)) {
                 return new HashSet<>(adjacencyList.values());
@@ -32,18 +32,18 @@ public class GraphAdjacencyListImpl implements GraphAdjacencyList<String> {
 
     @Override
     public void printAdjacencyList() {
-        for (Object key: this.adjacencyList.keySet()) {
-            System.out.println("\nNode " + key
+        for (Vertex key: this.adjacencyList.keySet()) {
+            System.out.println("\nNode " + key.getName()
                     + " makes an edge with ");
             this.adjacencyList.get(key).forEach((key1, value) -> System.out.println(
-                    "\tNode " + key1
+                    "\tNode " + key1.getName()
                             + " with edge weight "
                             + value + " "));
         }
     }
 
     @Override
-    public HashMap<String, HashMap<String, Integer>> get() {
+    public HashMap<Vertex, HashMap<Vertex, Integer>> get() {
         return adjacencyList;
     }
 }
