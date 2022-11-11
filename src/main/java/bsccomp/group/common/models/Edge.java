494bda5c0bc5a4ddcc1e877f3027e985d2554566
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-11-11 18:52:11 +0530

    add Edge

---
+++ src/main/java/bsccomp/group/common/models/Edge.java
@@ -0,0 +1,26 @@
+package bsccomp.group.common.models;
+
+public class Edge {
+    private int weight;
+    private boolean isIncluded = false;
+
+    public Edge(int weight) {
+        this.weight = weight;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public boolean isIncluded() {
+        return isIncluded;
+    }
+
+    public void setIncluded(boolean included) {
+        isIncluded = included;
+    }
+}
