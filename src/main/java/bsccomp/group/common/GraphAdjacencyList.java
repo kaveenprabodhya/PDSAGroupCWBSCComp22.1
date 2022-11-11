403f402117752ad074f7e9349374d1a6dfc82d97
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-11-11 18:52:46 +0530

    change Integer to Edge

--- src/main/java/bsccomp/group/common/GraphAdjacencyList.java
+++ src/main/java/bsccomp/group/common/GraphAdjacencyList.java
@@ -1,12 +1,14 @@
 package bsccomp.group.common;
 
+import bsccomp.group.common.models.Edge;
+
 import java.util.HashMap;
 import java.util.HashSet;
 
 public interface GraphAdjacencyList<T> {
     void addEdge(T node);
-    void addNodeAndWeightConnected(T vertex,T destinationNode, int weight);
+    void addNodeAndWeightConnected(T vertex,T destinationNode, Edge weight);
     HashSet<?> findAllDestinationNodeAndWeightsToAVertex(T node);
     void printAdjacencyList();
-    HashMap<T, HashMap<T, Integer>> get();
+    HashMap<T, HashMap<T, Edge>> returnList();
 }
