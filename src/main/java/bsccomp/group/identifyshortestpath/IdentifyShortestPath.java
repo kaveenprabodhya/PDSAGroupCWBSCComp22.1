cd4f5355e2b5d10cf7a59d77b41bb3ae71e54100
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-11-13 05:09:02 +0530

    updated with U generic

--- src/main/java/bsccomp/group/identifyshortestpath/IdentifyShortestPath.java
+++ src/main/java/bsccomp/group/identifyshortestpath/IdentifyShortestPath.java
@@ -2,12 +2,13 @@ package bsccomp.group.identifyshortestpath;
 
 import bsccomp.group.common.Base;
 import bsccomp.group.common.GraphAdjacencyList;
+import bsccomp.group.common.models.Edge;
 import bsccomp.group.common.models.Vertex;
 
 public class IdentifyShortestPath extends Base {
-    private final GraphAdjacencyList<Vertex> adjacencyList;
+    private final GraphAdjacencyList<Vertex, Edge> adjacencyList;
 
-    public IdentifyShortestPath(GraphAdjacencyList<Vertex> adjacencyList) {
+    public IdentifyShortestPath(GraphAdjacencyList<Vertex, Edge> adjacencyList) {
         this.adjacencyList = adjacencyList;
     }
 }
