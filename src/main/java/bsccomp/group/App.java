4bc468f54b67ad4db1d845f9ceb29096f5286892
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-11-20 05:11:00 +0530

    updated App

--- src/main/java/bsccomp/group/App.java
+++ src/main/java/bsccomp/group/App.java
@@ -2,14 +2,20 @@ package bsccomp.group;
 
 import bsccomp.group.manager.GameManager;
 
+import java.sql.SQLException;
+
 /**
  * 06-Nov-22
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        GameManager.run();
+    public static void main( String[] args ) throws SQLException, ClassNotFoundException {
+        GameManager.makeJDBCConnection();
+        GameManager gameManager = new GameManager();
+        gameManager.startFindMinimumConnectors();
+        System.out.println(gameManager.checkAnswersOfMinimumConnectors(gameManager.getAnswersForMinimumConnectors()));
+        GameManager.addMinimumConnectorsAnswersDataToDB(gameManager.getAnswersForMinimumConnectors(), "Kaveen");
+//        GameManager.connection.close();
     }
 }
