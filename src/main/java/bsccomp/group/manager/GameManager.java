75ad3393c59e6140d6326f83cfd2997b96513ae7
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-11-20 19:58:16 +0530

    updated GameManager

--- src/main/java/bsccomp/group/manager/GameManager.java
+++ src/main/java/bsccomp/group/manager/GameManager.java
@@ -1,22 +1,13 @@
 package bsccomp.group.manager;
 
 import bsccomp.group.common.GraphAdjacencyListImpl;
-import bsccomp.group.common.models.Edge;
-import bsccomp.group.common.models.Item;
-import bsccomp.group.common.models.Pair;
-import bsccomp.group.common.models.Vertex;
+import bsccomp.group.common.models.*;
 import bsccomp.group.identifyminimumconnectors.IdentifyMinimumConnectors;
 import bsccomp.group.identifyshortestpath.IdentifyShortestPath;
 import bsccomp.group.maximumprofit.FindMaximumProfit;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Objects;
+import java.sql.*;
+import java.util.*;
 
 public class GameManager {
     IdentifyMinimumConnectors minimumConnectors;
@@ -77,7 +68,9 @@ public class GameManager {
     }
 
     private static boolean isAllTrue(boolean[] array) {
-        for (boolean b : array) if (!b) return false;
+        for (boolean b : array)
+            if (!b)
+                return false;
         return true;
     }
 
@@ -115,10 +108,26 @@ public class GameManager {
         return shortestPath.getTable();
     }
 
+    public List<Pair<Vertex, Pair<Vertex, Pair<Integer, Boolean>>>> removedStarterSourceNode() {
+        List<Pair<Vertex, Pair<Vertex, Pair<Integer, Boolean>>>> table = shortestPath.getTable();
+        List<Pair<Vertex, Pair<Vertex, Pair<Integer, Boolean>>>> temp = new LinkedList<>();
+        for (Pair<Vertex, Pair<Vertex, Pair<Integer, Boolean>>> pair : table) {
+            if (!Objects.isNull(pair.getValue().getKey())) {
+                temp.add(pair);
+            }
+        }
+
+        return temp;
+    }
+
+    public Pair<Vertex, Pair<Vertex, Pair<Integer, Boolean>>> getSourceDataOfShortestPath() {
+        return shortestPath.getTable().get(0);
+    }
+
     public boolean checkAnswersOfShortestPath(List<Pair<Vertex, Pair<Vertex, Pair<Integer, Boolean>>>> pairList) {
-        boolean[] answers = new boolean[10];
+        boolean[] answers = new boolean[9];
         int count = 0;
-        for (Pair<Vertex, Pair<Vertex, Pair<Integer, Boolean>>> pairGenerated : getAnswersForShortestPath()) {
+        for (Pair<Vertex, Pair<Vertex, Pair<Integer, Boolean>>> pairGenerated : removedStarterSourceNode()) {
             for (Pair<Vertex, Pair<Vertex, Pair<Integer, Boolean>>> answerPair : pairList) {
                 if (pairGenerated.getKey().getName().equalsIgnoreCase(answerPair.getKey().getName())
                         && pairGenerated.getValue().getKey().getName().equalsIgnoreCase(answerPair.getValue().getKey().getName())
@@ -132,6 +141,10 @@ public class GameManager {
         return isAllTrue(answers);
     }
 
+    public void printShortestPathTable() {
+        shortestPath.printTable();
+    }
+
     public static void makeJDBCConnection() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
         connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/pdsa_cw", "root", "qwerty@123");
@@ -142,9 +155,9 @@ public class GameManager {
         }
     }
 
-    public static void addMinimumConnectorsAnswersDataToDB(List<Pair<Vertex, List<Pair<Vertex, Edge>>>> userAnswerList, String username) {
-        String query = "INSERT INTO minimumconnectors(Vertex, ToVertex, Edge, Username) VALUES (?,?,?,?)";
-
+    public static void addMinimumConnectorsAnswersDataToDB(List<Pair<Vertex, List<Pair<Vertex, Edge>>>> userAnswerList, User user) throws SQLException {
+        User createdUser = addUserDetailsDataToDB(user);
+        String query = "INSERT INTO minimumconnectors(vertex, toVertex, edge, user_id) VALUES (?,?,?,?)";
         try {
             for (Pair<Vertex, List<Pair<Vertex, Edge>>> answerListPair : userAnswerList) {
                 for (Pair<Vertex, Edge> answerEdgePair : answerListPair.getValue()) {
@@ -152,7 +165,7 @@ public class GameManager {
                     preparedStatement.setString(1, answerListPair.getKey().getName());
                     preparedStatement.setString(2, answerEdgePair.getKey().getName());
                     preparedStatement.setInt(3, answerEdgePair.getValue().getWeight());
-                    preparedStatement.setString(4, username);
+                    preparedStatement.setInt(4, createdUser.getUserId());
                     preparedStatement.executeUpdate();
                 }
             }
@@ -162,15 +175,19 @@ public class GameManager {
     }
 
     public static void addShortestPathAnswersDataToDB(List<Pair<Vertex, Pair<Vertex, Pair<Integer, Boolean>>>> userAnswerList,
-                                                      String username) {
-        String query = "INSERT INTO shortestpath VALUES (?,?,?,?)";
+                                                      User user) throws SQLException {
+        User createdUser = addUserDetailsDataToDB(user);
+        String query = "INSERT INTO shortestpath(vertex, prevVertex, distanceFromSrc, user_id) VALUES (?,?,?,?)";
         try {
             for (Pair<Vertex, Pair<Vertex, Pair<Integer, Boolean>>> answerListPair : userAnswerList) {
                 preparedStatement = connection.prepareStatement(query);
                 preparedStatement.setString(1, answerListPair.getKey().getName());
-                preparedStatement.setString(2, answerListPair.getValue().getKey().getName());
+                preparedStatement.setString(2, (Objects.isNull(answerListPair.getValue().getKey())
+                        ? null
+                        : answerListPair.getValue().getKey().getName())
+                );
                 preparedStatement.setInt(3, answerListPair.getValue().getValue().getKey());
-                preparedStatement.setString(4, username);
+                preparedStatement.setInt(4, createdUser.getUserId());
                 preparedStatement.executeUpdate();
             }
         } catch (SQLException e) {
@@ -178,15 +195,49 @@ public class GameManager {
         }
     }
 
-    public static void addMaximumProfitAnswersDataToDB(String maximumProfitAnswer, String username) {
-        String query = "INSERT INTO maximumprofit VALUES (?,?)";
+    public static void addMaximumProfitAnswersDataToDB(String maximumProfitAnswer, User user) throws SQLException {
+        User createdUser = addUserDetailsDataToDB(user);
+        String query = "INSERT INTO maximumprofit(maximum_profit, user_id) VALUES (?,?)";
         try {
             preparedStatement = connection.prepareStatement(query);
             preparedStatement.setString(1, maximumProfitAnswer);
-            preparedStatement.setString(2, username);
+            preparedStatement.setInt(2, createdUser.getUserId());
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static User addUserDetailsDataToDB(User user) throws SQLException {
+        boolean b = readUserDataFromDB(user);
+        if (!b) {
+            String query = "INSERT INTO users(username) VALUES (?)";
+            preparedStatement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            preparedStatement.setString(1, user.getUsername());
+            int i = preparedStatement.executeUpdate();
+            if (i == 0) {
+                throw new SQLException("Creating user failed, no rows affected.");
+            }
+            try (ResultSet generatedKeys = preparedStatement.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    user.setUserId(generatedKeys.getInt(1));
+                } else {
+                    throw new SQLException("Creating user failed, no ID obtained.");
+                }
+            }
+            return user;
+        }
+        return user;
+    }
+
+    private static boolean readUserDataFromDB(User user) throws SQLException {
+        String query = "SELECT id FROM users WHERE username = '" + user.getUsername() + "'";
+        preparedStatement = connection.prepareStatement(query);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        if (resultSet.next()) {
+            user.setUserId(resultSet.getInt("id"));
+            return true;
+        }
+        return false;
     }
 }
