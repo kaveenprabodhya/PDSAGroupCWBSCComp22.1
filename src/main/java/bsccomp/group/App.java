b27f44c4c018afcaf5fb8f7953fae9d8b5e19fc7
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-11-20 23:59:04 +0530

    commented code

--- src/main/java/bsccomp/group/App.java
+++ src/main/java/bsccomp/group/App.java
@@ -10,34 +10,34 @@ import java.sql.SQLException;
  */
 public class App {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        GameManager.makeJDBCConnection();
-        GameManager gameManager = new GameManager();
-
-        gameManager.startFindMinimumConnectors();
-        if (gameManager.checkAnswersOfMinimumConnectors(gameManager.getAnswersForMinimumConnectors())) {
-            GameManager.addMinimumConnectorsAnswersDataToDB(gameManager.getAnswersForMinimumConnectors(), new User("Kaveen"));
-        } else {
-            System.out.println("wrong answer");
-        }
-
-        gameManager.startFindMaximumProfit();
-        boolean b = gameManager.checkAnswersOfMaximumProfit(gameManager.getMaximumProfitFor10kgs());
-        System.out.println(gameManager.getMaximumProfitFor10kgs());
-        if (b) {
-            GameManager.addMaximumProfitAnswersDataToDB(String.valueOf(gameManager.getMaximumProfitFor10kgs()), new User("Kaveen"));
-        } else {
-            System.out.println("wrong answer");
-        }
-
-        gameManager.startFindShortestPath();
-        gameManager.printShortestPathTable();
-        boolean b1 = gameManager.checkAnswersOfShortestPath(gameManager.getAnswersForShortestPath());
-        if (b1) {
-            GameManager.addShortestPathAnswersDataToDB(gameManager.getAnswersForShortestPath(), new User("Kaveen"));
-        } else {
-            System.out.println("wrong answer");
-        }
-
-        GameManager.connection.close();
+//        GameManager.makeJDBCConnection();
+//        GameManager gameManager = new GameManager();
+//
+//        gameManager.startFindMinimumConnectors();
+//        if (gameManager.checkAnswersOfMinimumConnectors(gameManager.getAnswersForMinimumConnectors())) {
+//            GameManager.addMinimumConnectorsAnswersDataToDB(gameManager.getAnswersForMinimumConnectors(), new User("Kaveen"));
+//        } else {
+//            System.out.println("wrong answer");
+//        }
+//
+//        gameManager.startFindMaximumProfit();
+//        boolean b = gameManager.checkAnswersOfMaximumProfit(gameManager.getMaximumProfitFor10kgs());
+//        System.out.println(gameManager.getMaximumProfitFor10kgs());
+//        if (b) {
+//            GameManager.addMaximumProfitAnswersDataToDB(String.valueOf(gameManager.getMaximumProfitFor10kgs()), new User("Kaveen"));
+//        } else {
+//            System.out.println("wrong answer");
+//        }
+//
+//        gameManager.startFindShortestPath();
+//        gameManager.printShortestPathTable();
+//        boolean b1 = gameManager.checkAnswersOfShortestPath(gameManager.getAnswersForShortestPath());
+//        if (b1) {
+//            GameManager.addShortestPathAnswersDataToDB(gameManager.getAnswersForShortestPath(), new User("Kaveen"));
+//        } else {
+//            System.out.println("wrong answer");
+//        }
+//
+//        GameManager.connection.close();
     }
 }
