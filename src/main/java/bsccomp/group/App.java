4dc89c9606cf2cdafa2e39acc5e450b905d6035a
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-11-20 19:58:26 +0530

    updated App

--- src/main/java/bsccomp/group/App.java
+++ src/main/java/bsccomp/group/App.java
@@ -1,21 +1,46 @@
 package bsccomp.group;
 
+import bsccomp.group.common.models.Pair;
+import bsccomp.group.common.models.User;
+import bsccomp.group.common.models.Vertex;
 import bsccomp.group.manager.GameManager;
 
 import java.sql.SQLException;
+import java.util.Objects;
 
 /**
  * 06-Nov-22
- *
  */
-public class App 
-{
-    public static void main( String[] args ) throws SQLException, ClassNotFoundException {
+public class App {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
         GameManager.makeJDBCConnection();
         GameManager gameManager = new GameManager();
+
         gameManager.startFindMinimumConnectors();
-        System.out.println(gameManager.checkAnswersOfMinimumConnectors(gameManager.getAnswersForMinimumConnectors()));
-        GameManager.addMinimumConnectorsAnswersDataToDB(gameManager.getAnswersForMinimumConnectors(), "Kaveen");
-//        GameManager.connection.close();
+        if (gameManager.checkAnswersOfMinimumConnectors(gameManager.getAnswersForMinimumConnectors())) {
+            GameManager.addMinimumConnectorsAnswersDataToDB(gameManager.getAnswersForMinimumConnectors(), new User("Kaveen"));
+        } else {
+            System.out.println("wrong answer");
+        }
+
+        gameManager.startFindMaximumProfit();
+        boolean b = gameManager.checkAnswersOfMaximumProfit(gameManager.getMaximumProfitFor10kgs());
+        System.out.println(gameManager.getMaximumProfitFor10kgs());
+        if (b) {
+            GameManager.addMaximumProfitAnswersDataToDB(String.valueOf(gameManager.getMaximumProfitFor10kgs()), new User("Kaveen"));
+        } else {
+            System.out.println("wrong answer");
+        }
+
+        gameManager.startFindShortestPath();
+        gameManager.printShortestPathTable();
+        boolean b1 = gameManager.checkAnswersOfShortestPath(gameManager.getAnswersForShortestPath());
+        if (b1) {
+            GameManager.addShortestPathAnswersDataToDB(gameManager.getAnswersForShortestPath(), new User("Kaveen"));
+        } else {
+            System.out.println("wrong answer");
+        }
+
+        GameManager.connection.close();
     }
 }
